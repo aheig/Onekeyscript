fe87c1c2936b9d279f3e6b54ab39b74945e7b05b
aheig <61814388+aheig@users.noreply.github.com>
2021-10-23 02:22:52 +0800

    Update update-xray-4-openwrt.sh

--- update-xray-4-openwrt.sh
+++ update-xray-4-openwrt.sh
@@ -3,10 +3,11 @@
 
 #openwrt版
 wget -O /tmp/xray.tar.gz https://github.com/felix-fly/xray-openwrt/releases/latest/download/xray-linux-amd64.tar.gz
-#AMD64版
-#wget -O /tmp/xray.tar.gz https://github.com/XTLS/Xray-core/releases/latest/download/xray-linux-amd64.tar.gz
 tar -xzvf /tmp/xray.tar.gz
 rm -rf /tmp/xray.tar.gz
-mv xray /usr/bin/xray
+#AMD64版
+#wget -O /tmp/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/xray-linux-64.zip
 #unzip xray.zip /usr/bin/
+#rm -rf /tmp/xray.zip
+mv xray /usr/bin/xray
 chmod 755 /usr/bin/xray
