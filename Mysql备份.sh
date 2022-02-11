709f58939928578ade0ba80129b75d27d1d5e692
aheig <61814388+aheig@users.noreply.github.com>
2022-02-11 16:50:20 +0800

    Update Mysql备份.sh

--- Mysql备份.sh
+++ Mysql备份.sh
@@ -11,42 +11,42 @@ chmod -R 775 $BackupDir
 chmod -R 775 $SaveDir
 #备份database1库
 function database1 () {
-        echo "`date "+%m %d %H:%M:%S"` Begin backup database1 database" >> $BackupDir/backmysql.log
+        echo "`date "+%m %d %H:%M:%S"` 开始备份 database1 数据库" >> $BackupDir/backmysql.log
                 if [ ! -d $BackupDir/mysql ];then
                 mkdir -p $BackupDir/mysql
                 fi
                 chown -R mysql:mysql $BackupDir/mysql
         mysqldump -u root -p'YourMySQLPasswd'  database1 > $BackupDir/mysql/database1_$fdate.sql
-        echo "`date "+%m %d %H:%M:%S"` backup database1 database done" >> $BackupDir/backmysql.log
+        echo "`date "+%m %d %H:%M:%S"` 备份 database1 数据库完成" >> $BackupDir/backmysql.log
 }
 #备份database2库
 function database2 () {
-        echo "`date "+%m %d %H:%M:%S"` Begin backup database2 database" >> $BackupDir/backmysql.log
+        echo "`date "+%m %d %H:%M:%S"` 开始备份 database2 数据库" >> $BackupDir/backmysql.log
                 if [ ! -d $BackupDir/mysql ];then
                 mkdir -p $BackupDir/mysql
                 fi
                 chown -R mysql:mysql $BackupDir/mysql
         mysqldump -u root -p'YourMySQLPasswd' database2 > $BackupDir/mysql/database2_$fdate.sql
-        echo "`date "+%m %d %H:%M:%S"` backup database2 database done"  >> $BackupDir/backmysql.log
+        echo "`date "+%m %d %H:%M:%S"` 备份 database2 数据库完成"  >> $BackupDir/backmysql.log
 }
 #备份database3库
 function database3 () {
-        echo "`date "+%m %d %H:%M:%S"` Begin backup database3 database" >> $BackupDir/backmysql.log
+        echo "`date "+%m %d %H:%M:%S"` 开始备份 database3 数据库" >> $BackupDir/backmysql.log
                 if [ ! -d $BackupDir/mysql ];then
                 mkdir -p $BackupDir/mysql
                 fi
                 chown -R mysql:mysql $BackupDir/mysql
         mysqldump -u root -p'YourMySQLPasswd' database3 > $BackupDir/mysql/database3_$fdate.sql
-        echo "`date "+%m %d %H:%M:%S"` backup database3 database done"  >> $BackupDir/backmysql.log
+        echo "`date "+%m %d %H:%M:%S"` 备份 database3 数据库完成"  >> $BackupDir/backmysql.log
 }
 #压缩删除备份文件
 function compress () {
-        echo "`date "+%m %d %H:%M:%S"` Begin complete database " >> $BackupDir/backmysql.log
+        echo "`date "+%m %d %H:%M:%S"` 开始压缩完整的数据库 " >> $BackupDir/backmysql.log
                 chown -R mysql:mysql $BackupDir
                 cd $BackupDir
                 tar -cf mysql_$fdate.tar mysql
                 if [[ $? -eq 0 && -s mysql_$fdate.tar ]];then
-                echo "`date "+%m %d %H:%M:%S"` data compress done" >> $BackupDir/backmysql.log
+                echo "`date "+%m %d %H:%M:%S"` 完成数据库压缩" >> $BackupDir/backmysql.log
                 find $BackupDir -mtime +3 -exec rm -rf{} \;
                 fi
                 if [ ! -d $SaveDir ];then
@@ -54,7 +54,7 @@ function compress () {
                 fi
                 cd $BackupDir
                 \cp mysql_$fdate.tar $SaveDir
-                echo "`date "+%m %d %H:%M:%S"` cpoy data to savedir done" >> $BackupDir/backmysql.log
+                echo "`date "+%m %d %H:%M:%S"` 备份数据到 Savedir 完成" >> $BackupDir/backmysql.log
                 #判断备份文件数是否大于3
                 nuf=`find $SaveDir -name '*.tar' -size +10M|wc -l`
                 if [[ $? -eq 0 && $nuf -gt 3 ]];then
